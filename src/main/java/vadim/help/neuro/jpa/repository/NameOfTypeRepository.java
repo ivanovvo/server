29dbfa257fdaea8ba47343f7a336bdf1a72ddca4
ivanovvo <ivanov.vo@edu.spbstu.ru>
2021-04-12 09:26:47 +0300

    SErver of encyclopedia

--- src/main/java/vadim/help/neuro/jpa/repository/NameOfTypeRepository.java
+++
@@ -1,9 +0,0 @@
-package vadim.help.neuro.jpa.repository;
-
-import org.springframework.data.repository.CrudRepository;
-import org.springframework.stereotype.Repository;
-import vadim.help.neuro.jpa.models.NameOfType;
-
-@Repository
-public interface NameOfTypeRepository extends CrudRepository<NameOfType, Long> {
-}
